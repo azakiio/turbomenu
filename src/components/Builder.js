93fdff33f812c3d62b4e4558e70fc9eca29ca256
azakiio <azakica@gmail.com>
2020-08-03 19:42:58 -0400

    Add more to builder

--- src/components/Builder.js
+++ src/components/Builder.js
@@ -1,22 +1,77 @@
 import React, { useState } from "react";
+import { FaChevronRight, FaEllipsisH } from "react-icons/fa";
+import { GrDrag } from "react-icons/gr";
 
 function Builder(props) {
-    const block = "builder";
-    const [menu, setMenu] = useState({});
+  const block = "builder";
+  const [menu, setMenu] = useState([]);
+  const [isSectionInput, setSectionInput] = useState(false);
 
-    return (
-        <section className={block}>
-            <h2 className={block + "__title"}>Menu</h2>
-            {Object.keys(menu).length === 0 && <p>Start building your menu by adding your first menu section.</p>}
-            <div className={block + "__menu"}>
-                
-            </div>
-            <button>+ Add Menu section</button>
+  function addSection(event) {
+    event.preventDefault();
+    setMenu([...menu, { name: event.target.name.value, description: event.target.description.value }]);
+  }
 
+  return (
+    <main className={block}>
+      <h2 className={block + "__title"}>Menu</h2>
+      {!menu.length && <p>Start building your menu by adding your first menu section.</p>}
+      <div className={block + "__menu"}>
+        {menu.map((item, index) => (
+          <Section key={index} name={item.name} description={item.description} />
+        ))}
+      </div>
+      {isSectionInput && <SectionInput handleSubmit={addSection} />}
+      {!isSectionInput && <button onClick={() => setSectionInput(!isSectionInput)}>+ Add Menu section</button>}
+    </main>
+  );
+}
 
-        </section>
+function Section(props) {
+  const block = "section";
+  return (
+    <section className={block}>
+      <div className={block + "__header"}>
+        <div className={block + "__left"}>
+          <div className={block + "__buttons"}>
+            <GrDrag />
+            <FaChevronRight />
+            {props.name}
+          </div>
+          <div className={block + "__description"}>{props.description}</div>
+        </div>
 
-    );
+        <div className={block + "__right"}>
+          <FaEllipsisH />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function SectionInput(props) {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <label>
+        Name <input name="name" type="text" required />
+      </label>
+      <label>
+        Description (Optional): <input name="description" type="text" />
+      </label>
+      <button type="submit">Add</button>
+      <button type="reset">Cancel</button>
+    </form>
+  );
 }
 
 export default Builder;
+
+// function MenuItem(props) {
+
+//     return (
+//     <li>
+//         <div>{props.name}</div>
+//         <div>{props.description}</div>
+//         <div>{props.price}</div>
+//     </li>);
+// }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,34 @@
 import React from "react";
 import QRCode from "qrcode.react";
-import { FaDownload, FaPrint } from 'react-icons/fa';
+import { FaDownload, FaPrint } from "react-icons/fa";
 
 function Header(props) {
-  const block = "header"
+  const block = "header";
 
   return (
     <header className={block}>
-      <nav className={block + '__nav'}>
-        <a href="/" className={block + '__logo'}>
+      <nav className={block + "__nav"}>
+        <a href="/" className={block + "__logo"}>
           TurboMenu
         </a>
         <div className={block + "__right"}>
-          <a className={block + "__button"} href="/">Account</a>
-          <a className={block + "__button"} href="/">Logout</a>
+          <a className={block + "__action"} href="/">
+            Account
+          </a>
+          <a className={block + "__action"} href="/">
+            Logout
+          </a>
         </div>
       </nav>
       <div className={block + "__restaurant"}>
         <h1 className={block + "__title"}>{props.restaurant}</h1>
-        <p classNamer={block + "__link"}>{props.link}</p>
-        <div>
+        <p className={block + "__link"}>{props.link}</p>
+        <div className={block + "__qr-container"}>
           <QRCode value={props.link} />
-          <div>
-            <button><FaDownload /> Download</button>
-            <button><FaPrint /> Print</button>
-          </div>
+          <button className={block + "__action"}>
+            <FaDownload /> Download
+          </button>
         </div>
-
       </div>
     </header>
   );
